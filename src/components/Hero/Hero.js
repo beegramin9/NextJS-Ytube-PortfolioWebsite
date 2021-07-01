40f29fe7c6af29a867ad802e196a578ef03bc91b
wontae <beegramin9@gmail.com>
2021-07-01 13:55:15 +0900

    feat: Hero, SVG out of the box

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,8 +5,21 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <Section row nopadding>
-    <LeftSection></LeftSection>
+  <Section grid nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there! <br/>
+        I am Wontae Choi.
+      </SectionTitle>
+      <SectionText>
+      I am an enthusiastic software developer eager to learn new skills and
+      technologies, also knowledgeable in software applications, algorithms,
+      development techniques, and data structures.
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://mail.google.com'}>
+        Learn more
+      </Button>
+    </LeftSection>
   </Section>
 );
 
