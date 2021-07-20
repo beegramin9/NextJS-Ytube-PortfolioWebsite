acb7a16f0d3154942a3e949de09fac476f97f274
wontae <beegramin9@gmail.com>
2021-07-20 22:29:55 +0900

    adjusting colors

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const arrayOfProject = [
   {
     title: 'Momentum Clone',
     description: "This project started from a plain CSS-less todo-list tutorial, improved after to be a Momentum Clone. The most well-marked new feature is the separated pending and finished list. where todos are transferable onto another section, data persistence supported by Local Storage. Other new features are Alert, Weather and background image API which will be reusable at any needed time, name form and clock with cool CSS.",
-    image: '/images/portfolio/Momentum.gif',
+    image: '/images/projects/Momentum.gif',
     tags: ['https://img.shields.io/badge/LocalStorage-FF3621?style=flat-square&logo=Databricks&logoColor=white',
           'https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white', 
           'https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white', 
@@ -15,7 +15,7 @@ export const arrayOfProject = [
   {
     title: 'Paintbrush Clone',
     description: "Realizing how much of a skill booster cloning practice is, I wanted to continue on cloning projects but needed a unique one that would make my work stand out. This clone is a mock-up of Window paint brush, made with MDN Canvas API. Main features are drawing, filling, erasing and reseting, all of which are interactive with the colors off of the buttons.",
-    image: '/images/portfolio/Paintbrush.gif',
+    image: '/images/projects/Paintbrush.gif',
     tags: ['https://img.shields.io/badge/MDNCanvas-01756c?style=flat-square&logo=MDN Web Docs&logoColor=white',
     'https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white', 
     'https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white', 
@@ -27,7 +27,7 @@ export const arrayOfProject = [
   {
     title: 'Calculator Clone',
     description: "The calculator functionality is pulled through by Javascript ES6 Class, which made easier to store and call the values and operator functions the separately. The number blocks and button blocks are constructed within CSS3 grid, flex, which made easier to remain intact when the web moves responsively.",
-    image: '/images/portfolio/Calculator.gif',
+    image: '/images/projects/Calculator.gif',
     tags: ['https://img.shields.io/badge/ES6-004027?style=flat-square&logo=JSON&logoColor=white',
     'https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white', 
     'https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white', 
@@ -39,7 +39,7 @@ export const arrayOfProject = [
   {
     title: 'Movie Archive Webapp',
     description: "This project works on a movie list api, added with custom responsive design. It follows the conventional React folder structure which makes it scalable and sustainable. There are many reusable features such as custom FetchAPIandPagination Hook, styled components, routers and loading spinner. The social media icons at the header and footer lead to my social accounts, and About page contains a prologue of this project.",
-    image: '/images/portfolio/React.gif',
+    image: '/images/projects/React.gif',
     tags: ['https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=white', 
           'https://img.shields.io/badge/StyledComponents-DB7093?style=flat-square&logo=Styled-Components&logoColor=white', 
           'https://img.shields.io/badge/Sass-CC6699?style=flat-square&logo=Sass&logoColor=white'],
--- src/themes/default.js
+++ src/themes/default.js
@@ -6,9 +6,19 @@ const theme = {
     main: "Space Grotesk, sans-serif"
   },
   // Colors for layout
+  //! primary1: hsl(204,23.8%,95.9%)
+  //! background1: #0F1624
+  //! accent1: hsl(34.9,98.6%,72.9%)
+  //! button: hsl(205.1,100%,36.1%)
+  //! background2: hsl(232.7,27.3%,23.7%)
+
+  //todo background: rgb(242, 242, 242), skyblue: rgb(135, 209, 210)
+  //todo black: rgb(33, 28, 24), darkgreen: rgb(72, 98, 74), lightblack: rgb(70, 65, 61)
+  //todo red: rgb(252, 158, 148), yellow: rgb(243, 210, 179)
+
   colors: {
-    primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
+    primary1: "rgb(242, 242, 242)",
+    background1: "rgb(242, 242, 242)", 
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
