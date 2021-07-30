6adf726805ef463b4e03c1088fb6c2e13f6bc734
wontae <beegramin9@gmail.com>
2021-07-30 15:49:16 +0900

    card padding

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -20,7 +20,7 @@ export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  padding: 0 0 1rem;
+  padding: 0 2 1rem;
 `;
 
 /* @media ${(props) => props.theme.breakpoints.sm} {
