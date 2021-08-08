d95b845ae770ff00dba617bab77da0e4cd5f22c3
wontae <beegramin9@gmail.com>
2021-08-08 19:35:07 +0900

    a tag blocked

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -22,13 +22,22 @@ const GlobalStyles = createGlobalStyle`
     background: ${props => props.theme.colors.background1};
     color: ${props => props.theme.colors.primary1};
     cursor: default;
-
   }
   h1,h2,h3,h4,h5,h6,button {
     font-family: ${props => props.theme.fonts.title};
   }
   a {
     text-decoration: none;
+
+    -webkit-touch-callout:none; /* 롱탭일 시 팝업창 차단하기 */
+    
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0); /* 롱탭 배경 파란색으로 하이라이트되는 것 막기 */
+    
+    // -webkit-user-select: none; /* 텍스트/이미지 선택 불가 => 확대 불가 */
+    // -ms-user-select: none;
+    // -moz-user-select: none;
+    // -webkit-user-select: none;
+    // -user-select: none;
   }
   li{
     list-style: none;
